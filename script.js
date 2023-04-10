f848c4eaeac3aaea640d2b9184392f2a19211e7d
Gustavo Lima <gustavo.lima@mj.gov.br>
2023-04-10 17:10:19 -0300

    Aprimoramento

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-let projetos = [
+const projetos = [
     {
         titulo: 'Meus Gastos',
         github: 'https://github.com/GustavodSouza/MeusGastos',
         site: 'https://meus-gastos.vercel.app/',
         img: './img/meus-gastos.png',
-        descricao: 'Este projeto é sobre controle financeiro',
-        tecnologias: 'Angular - Typescript - Sass - Firebase'
+        descricao: 'Este é meu principal projeto no momento, desenvolvido a partir de uma necessidade minha '
+        + 'de armazenar meus gastos mensais e obter o total de gastos a fim de aumentar minhas economias. O projeto foi desenvolvido utilizando Angular '
+        + 'junto ao Firebase como banco de dados. O sistema conta com PWA o que permite instalar na sua máquina ou celular pessoal, facilitando seu acesso.'
+        + 'O projeto ainda está sendo aprimorado.',
+        tecnologias: 'Angular - Typescript - PWA - Sass - Firebase'
     },
     {
         titulo: 'Tasks React',
         github: 'https://github.com/GustavodSouza/Tasks-react.git',
-        site: 'www.google.com.br',
-        img: '',
-        descricao: 'Cadastramento de tarefas em React',
+        site: 'tasks-react-gustavo.vercel.app',
+        img: './img/tasks-react.png',
+        descricao: 'Projeto realizado com a finalidade de aprender React e armazenar uma lista de tarefas, sendo possível marcar como feita.'
+        + 'Neste projeto é possível realizar um CRUD.',
         tecnologias: 'React - Javascript'
     },
     {
-        titulo: 'Meus Gastos',
-        github: 'https://github.com/GustavodSouza/MeusGastos',
-        site: 'https://meus-gastos.vercel.app/',
-        img: './img/meus-gastos.png',
-        descricao: 'Este projeto é sobre controle financeiro',
-        tecnologias: 'Angular - Typescript - Sass - Firebase'
+        titulo: 'Teste',
+        github: 'https://github.com/GustavodSouza/Tasks-react.git',
+        site: '#',
+        img: '#',
+        descricao: 'Aqui será os próximos projetos',
+        tecnologias: 'Em alguma linguagem qualquer'
     }
 ];
 
