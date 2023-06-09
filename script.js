9c74b66e9778034082718b344a452ee98f4d1ef0
Gustavo Lima <gustavo.lima@mj.gov.br>
2023-06-09 13:44:13 -0300

    Ajustes visuais

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let projetos = [
+ let projetos = [
   {
     titulo: "Meus Gastos",
     github: "https://github.com/GustavodSouza/MeusGastos",
     site: "https://meus-gastos.vercel.app/",
     img: "./img/projetos/meus-gastos.png",
     descricao: "Projeto pessoal desenvolvido para controlar os gastos mensais.",
-    tecnologias: "Angular - Typescript - PWA - Sass - Firebase",
+    tecnologias: "Angular - Typescript - Scss - Firebase - Pwa",
   },
   {
     titulo: "Tasks React",
@@ -35,11 +35,11 @@ let charIndex = 0;
 let botaoTopo = document.getElementById("botao-topo");
 const textoSpan = document.querySelector(".texto");
 const cursorSpan = document.querySelector(".cursor");
+const botaoVerMais = document.getElementById("div-botao-ver-mais");
 const textArray = [
   "Desenvolvedor Front-end",
   "um entusiasta da tecnologia",
   "dedicado e comprometido",
-  "o que sua empresa precisa!",
 ];
 const typingDelay = 100;
 const erasingDelay = 50;
@@ -47,61 +47,64 @@ const newTextDelay = 2000;
 const card = document.getElementById("projetos-position");
 
 carregarProjetos = (filtrados) => {
-  let data = !filtrados ? projetos : filtrados;
-  document.getElementById("div-botao-ver-mais").style =
-    data.length > 2 ? "display: flex" : "display: none";
-  data.forEach((item) => {
-    contador++;
-    let projeto = "";
-    let isPar = contador % 2 == 0;
-    let display = contador > 2 ? "hidden" : "show";
+  const data = filtrados ? filtrados : projetos;
+  
+  botaoVerMais.style = data.length > 2 ? "display: flex" : "display: none";
 
-    if (isPar) {
-      projeto = `
-              <div class="card card-right ${display}">
-                  <div class="projetos-descricao-right">
-                      <h1>${item.titulo}</h1>
-                      <p>${item.descricao}</p>
-                      <p class="bold">${item.tecnologias}</p>
-                      <div class="botoes-projeto">
-                          <a href="${item.site}" target="_blank"><img src="./img/visitar.png" alt="Visualizar projeto" loading="lazy" /></a>
-                          <a href="${item.github}" target="_blank"><img src="./img/repositorio.png" alt="Abrir repositório do projeto" loading="lazy" /></a>
+    if (data.length > 0) {
+      data.forEach((item) => {
+        contador++;
+        let projeto = "";
+        const isPar = contador % 2 == 0;
+        const display = contador > 2 ? "hidden" : "show";
+    
+        if (isPar) {
+          projeto = `
+                  <div class="card card-right ${display}">
+                      <div class="projetos-descricao-right">
+                          <h1>${item.titulo}</h1>
+                          <p>${item.descricao}</p>
+                          <p class="bold">${item.tecnologias}</p>
+                          <div class="botoes-projeto">
+                              <a href="${item.site}" target="_blank"><img class="icon" id="visitar" src="./img/visitar.png" alt="Visualizar projeto" loading="lazy" /></a>
+                              <a href="${item.github}" target="_blank"><img class="icon" id="repositorio" src="./img/repositorio.png" alt="Abrir repositório do projeto" loading="lazy" /></a>
+                          </div>
                       </div>
-                  </div>
-                  <div class="projetos-img">
-                      <img src="${item.img}" alt="" />
-                  </div>
-              </div>`;
+                      <div class="projetos-img">
+                          <img src="${item.img}" alt="" />
+                      </div>
+                  </div>`;
+        } else {
+          projeto = `
+                  <div class="card card-left ${display}">
+                      <div class="projetos-img">
+                          <img src="${item.img}" alt="" />
+                      </div>
+                      <div class="projetos-descricao-left">
+                          <h1>${item.titulo}</h1>
+                          <p>${item.descricao}</p>
+                          <p class="bold">${item.tecnologias}</p>
+                          <div class="botoes-projeto">
+                              <a href="${item.site}" target="_blank"><img class="icon" id="visitar" src="./img/visitar.png" alt="Visualizar projeto" loading="lazy" /></a>
+                              <a href="${item.github}" target="_blank"><img class="icon" id="repositorio" src="./img/repositorio.png" alt="Abrir repositório do projeto" loading="lazy" /></a>
+                          </div>
+                      </div>
+                  </div>`;
+        }
+    
+        card.innerHTML += projeto;
+      });
+      
     } else {
-      projeto = `
-              <div class="card card-left ${display}">
-                  <div class="projetos-img">
-                      <img src="${item.img}" alt="" />
-                  </div>
-                  <div class="projetos-descricao-left">
-                      <h1>${item.titulo}</h1>
-                      <p>${item.descricao}</p>
-                      <p class="bold">${item.tecnologias}</p>
-                      <div class="botoes-projeto">
-                          <a href="${item.site}" target="_blank"><img src="./img/visitar.png" alt="Visualizar projeto" loading="lazy" /></a>
-                          <a href="${item.github}" target="_blank"><img src="./img/repositorio.png" alt="Abrir repositório do projeto" loading="lazy" /></a>
-                      </div>
-                  </div>
-              </div>`;
+      card.innerHTML = '<h3>Nenhum projeto encontrado com esta tecnologia!</h3>';
     }
-
-    card.innerHTML += projeto;
-  });
+  
 };
 
 carregarProjetos();
 
 addEventListener("scroll", () => {
-  if (window.scrollY > 80) {
-    botaoTopo.style = "visibility: visible";
-  } else {
-    botaoTopo.style = "visibility: hidden";
-  }
+  botaoTopo.style = window.scrollY > 80 ? "visibility: visible" : "visibility: hidden";
 });
 
 input.addEventListener("input", () => {
@@ -110,23 +113,20 @@ input.addEventListener("input", () => {
   if (input.value) {
     click = false;
     document.getElementById("botao-ver-mais").textContent = "Ver mais";
-    let filtrados = projetos.filter(
-      (item) =>
-        item.tecnologias.toLowerCase().indexOf(input.value.toLowerCase()) > -1
-    );
+    const filtrados = projetos.filter((item) => item.tecnologias.toLowerCase().indexOf(input.value.toLowerCase()) > -1);
     carregarProjetos(filtrados);
-  } else {
-    carregarProjetos();
+    return;
   }
+  carregarProjetos();
 });
 
 verMaisProjetos = () => {
   click = !click;
 
-  let elems = document.querySelectorAll(".hidden");
+  const elems = document.querySelectorAll(".hidden");
 
-  for (let i = 0; i < elems.length; i++) {
-    elems[i].style.display = click ? "flex" : "none";
+  for (const elem of elems) {
+    elem.style.display = click ? "flex" : "none";
   }
 
   document.getElementById("botao-ver-mais").textContent = click
@@ -142,10 +142,10 @@ escreverTexto = () => {
     textoSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
     charIndex++;
     setTimeout(escreverTexto, typingDelay);
-  } else {
-    cursorSpan.classList.remove("typing");
-    setTimeout(apagarTexto, newTextDelay);
+    return;
   }
+  cursorSpan.classList.remove("typing");
+  setTimeout(apagarTexto, newTextDelay);
 };
 
 apagarTexto = () => {
@@ -159,12 +159,12 @@ apagarTexto = () => {
     );
     charIndex--;
     setTimeout(apagarTexto, erasingDelay);
-  } else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-    setTimeout(escreverTexto, typingDelay + 1100);
+    return;
   }
+  cursorSpan.classList.remove("typing");
+  textArrayIndex++;
+  if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+  setTimeout(escreverTexto, typingDelay + 1100);
 };
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -176,19 +176,31 @@ themeMode = () => {
   const html = document.getElementsByTagName("html");
   const imgModo = document.getElementById("modo");
   const themeMode = document.querySelectorAll('.theme-mode');
+  const icon = document.querySelectorAll('.icon');
 
   if (clickMode) {
     html[0].style = "background-color: var(--dark-mode); color: var(--branco)";
     imgModo.setAttribute("src", "./img/light-mode.png");
     botaoTopo.setAttribute('src', './img/seta-light.png');
 
-    for (let i = 0; i < themeMode.length; i++) {
-      themeMode[i].style.color = 'color: var(--branco);';
+    for (const element of icon) {
+      if (element.id === 'repositorio') {
+        element.setAttribute('src', './img/repositorio-light.png');
+      }
+
+      if (element.id === 'visitar') {
+        element.setAttribute('src', './img/visitar-light.png');
+      }
     }
 
-  } else {
-    html[0].style = "background-color: var(--branco); color: var(--secundario)";
-    imgModo.setAttribute("src", "./img/dark-mode.png");
-    botaoTopo.setAttribute('src', './img/seta-dark.png');
+    return;
+
   }
+  html[0].style = "background-color: var(--branco); color: var(--secundario)";
+  imgModo.setAttribute("src", "./img/dark-mode.png");
+  botaoTopo.setAttribute('src', './img/seta-dark.png');
 };
+
+abrirMenuMobile = () => {
+  
+}
