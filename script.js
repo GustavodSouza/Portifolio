10e6f2b6d5f4c834f7de8eb493894bc9b464bcd2
Gustavo de Lima <gustavo.delima@castgroup.com.br>
2024-09-16 16:39:52 -0300

    Novo portifólio

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
- let projetos = [
+let projetos = [
   {
     titulo: "Meus Gastos",
     github: "https://github.com/GustavodSouza/MeusGastos",
     site: "https://meus-gastos.vercel.app/",
     img: "assets/meus-gastos2.png",
-     descricao: "Projeto pessoal desenvolvido para controlar os gastos mensais. Neste projeto é possível registrar os gastos e fazer consultas obtendo o valor total por mês.",
+    descricao: "Projeto pessoal desenvolvido para controlar os gastos mensais. Neste projeto é possível registrar os gastos e fazer consultas obtendo o valor total por mês.",
     tecnologias: "Angular - Typescript - Scss - Firebase - Pwa",
   },
   {
@@ -24,66 +24,80 @@
     descricao: "Portifólio criado para mostrar meus principais projetos construídos no decorrer da minha carreira como programador.",
     tecnologias: "Html - Css - Javascript",
   },
+  {
+    titulo: "Portifólio",
+    github: "https://github.com/GustavodSouza/Portifolio.git",
+    site: "https://gustavosouzaportifolio.vercel.app/",
+    img: "assets/portifolio.png",
+    descricao: "Portifólio criado para mostrar meus principais projetos construídos no decorrer da minha carreira como programador.",
+    tecnologias: "Html - Css - Javascript",
+  },
+  {
+    titulo: "Portifólio",
+    github: "https://github.com/GustavodSouza/Portifolio.git",
+    site: "https://gustavosouzaportifolio.vercel.app/",
+    img: "assets/portifolio.png",
+    descricao: "Portifólio criado para mostrar meus principais projetos construídos no decorrer da minha carreira como programador.",
+    tecnologias: "Html - Css - Javascript",
+  },
 ];
 
 let click = false;
-let contador = 0;
 let input = document.getElementById("clrinput");
-const botaoVerMais = document.getElementById("div-botao-ver-mais");
+const botaoVerMais = document.getElementById("ver-mais");
 const card = document.getElementById("projetos-card");
 
 carregarProjetos = (filtrados) => {
   const data = filtrados ? filtrados : projetos;
-  
-    botaoVerMais.style = data.length > 2 ? "display: flex" : "display: none";
 
-    if (data.length > 0) {
-      data.forEach((item) => {
-        contador++;
-        let projeto = '';
-        const display = contador > 2 ? "hidden" : "show";
+  botaoVerMais.style.display = data.length > 4 ? '' : 'none';
 
-        projeto = `
-          <div class="projeto ${display}" data-aos="fade-up" data-aos-delay="350">
-            <img class="imagem-projeto" src="${item.img}" alt="Prévia do projeto" loading="lazy" />
-            <div class="description-projeto">
-              <h2>${item.titulo}</h2>
-              <p>${item.descricao}</p>
-              <strong>${item.tecnologias}</strong>
-              <div class="buttons-card">
-                  <a href="${item.github}" target="_blank">
-                      Repositório
-                      <i class="fa-brands fa-github"></i>
-                  </a>
-                  <a href="${item.site}" target="_blank">
-                      Visitar projeto
-                      <i class="fa-solid fa-eye"></i>
-                  </a>
-              </div>
-          </div>
-          `;
+  if (data.length > 0) {
+    data.forEach((item, index) => {
+      const display = index > 3 ? "hidden" : "show"
 
-        card.innerHTML += projeto;
-      });
-    } else {
-      card.innerHTML = '<strong>Nenhum projeto encontrado com esta tecnologia!</strong>';
+      card.innerHTML += `
+        <div class="col-md-5 col-sm-12 ${display}" style="border: 1px solid white;">
+            <div class="column">
+                <img style="width: 100%; height: 300px" class="imagem-projeto" src="${item.img}" alt="Prévia do projeto" loading="lazy" />
+                <p class="h4 text-center">${item.titulo}</p>
+                <p>${item.descricao}</p>
+                <strong>${item.tecnologias}</strong>
+                <div class="pt-3">
+                   <a href="${item.github}" target="_blank">
+                       Repositório
+                       <i class="fa-brands fa-github"></i>
+                   </a>
+                   <a href="${item.site}" target="_blank">
+                       Visitar projeto
+                       <i class="fa-solid fa-eye"></i>
+                   </a>
+               </div>
+            </div>
+        </div>`;
+    });
+    const elems = document.querySelectorAll(".hidden");
+    for (const elem of elems) {
+      elem.style.display = "none";
     }
+  } else {
+    card.innerHTML = '<strong>Nenhum projeto encontrado com esta tecnologia!</strong>';
+  }
 };
 
 carregarProjetos();
 
 input.addEventListener("input", () => {
-  contador = 0;
   card.innerHTML = "";
+
   if (input.value) {
     click = false;
-    document.getElementById("botao-ver-mais").textContent = "Ver mais";
-    const filtrados = projetos.filter((item) => item.tecnologias.toLowerCase().indexOf(input.value.toLowerCase()) > -1 || 
-    item.titulo.toLowerCase().indexOf(input.value.toLowerCase()) > -1);
+    const inputSearch = input.value.toLowerCase()
+    const filtrados = projetos.filter((item) => item.tecnologias.toLowerCase().match(inputSearch) || item.titulo.toLowerCase().match(inputSearch))
     carregarProjetos(filtrados);
-    return;
+  } else {
+    carregarProjetos();
   }
-  carregarProjetos();
 });
 
 verMaisProjetos = () => {
@@ -95,7 +109,7 @@ verMaisProjetos = () => {
     elem.style.display = click ? "flex" : "none";
   }
 
-  document.getElementById("botao-ver-mais").textContent = click
+  botaoVerMais.textContent = click
     ? "Ver menos"
     : "Ver mais";
 };
